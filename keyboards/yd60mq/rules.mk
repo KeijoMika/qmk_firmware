3a541c7cf770571d73a8f352e59915856180c775
Mika Salmela <mtsalmel84@gmail.com>
2021-12-26 14:47:42 +0200

    Rules.mk YD60MQ update

--- keyboards/yd60mq/rules.mk
+++ keyboards/yd60mq/rules.mk
@@ -15,15 +15,8 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-<<<<<<< HEAD
-MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-DEBOUNCE_TYPE = sym_eager_pk
-=======
 AUDIO_ENABLE = no           # Audio output
-
->>>>>>> upstream
+DEBOUNCE_TYPE = sym_eager_pk
 LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_ansi_tsangan 60_hhkb 60_iso 60_iso_split_bs_rshift 60_iso_tsangan
 
 DEFAULT_FOLDER = yd60mq/12led
